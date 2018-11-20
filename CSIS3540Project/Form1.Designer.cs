68ff9dde0a898d03315228ce52567b30fbd33463
RogTorr <40044717+RogTorr@users.noreply.github.com>
2018-11-20 14:10:43 -0800

    files added to CSIS3540Project

---
+++ CSIS3540Project/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CSIS5340Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.roomsGridView = new System.Windows.Forms.DataGridView();
+            this.studentsGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.roomsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // roomsGridView
+            // 
+            this.roomsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.roomsGridView.Location = new System.Drawing.Point(60, 67);
+            this.roomsGridView.Name = "roomsGridView";
+            this.roomsGridView.Size = new System.Drawing.Size(1007, 310);
+            this.roomsGridView.TabIndex = 0;
+            this.roomsGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.roomsGridView_CellClick);
+            // 
+            // studentsGridView
+            // 
+            this.studentsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentsGridView.Location = new System.Drawing.Point(60, 477);
+            this.studentsGridView.Name = "studentsGridView";
+            this.studentsGridView.Size = new System.Drawing.Size(1007, 153);
+            this.studentsGridView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.studentsGridView);
+            this.Controls.Add(this.roomsGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.roomsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView roomsGridView;
+        private System.Windows.Forms.DataGridView studentsGridView;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSIS3540Project/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSIS3540Project
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+        public double CellNumber { get; set; }
+        public string Email { get; set; } = "";
+        public string Room { get; set; } = "";
+    }
+}
